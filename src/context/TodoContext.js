789720fa4157ecfdfea508466aca54ffa3b43f3b
hexaalif <shadullahsad9@gmail.com>
2024-09-05 16:22:24 +0600

    project complete

---
+++ src/context/TodoContext.js
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { createContext } from "react";
+
+export const TodoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo: "todo msg",
+      completed: false,
+    },
+  ],
+  addTodo: () => {},
+  updateTodo: () => {},
+  deleteTodo: () => {},
+  toggleCompleted: () => {},
+});
+
+export const useTodo = () => {
+  return useContext(TodoContext);
+};
+
+export const TodoProvider = TodoContext.Provider;
